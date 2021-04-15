8c2a74b0a5588c37d0df12579863081ed00acd43
didee <41328920+Mainaderiq@users.noreply.github.com>
2021-04-15 20:47:38 +0300

    prep for evolution

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_platina.mk
+    $(LOCAL_DIR)/evolution_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_platina-user \
-    lineage_platina-userdebug \
-    lineage_platina-eng
+    evolution_platina-user \
+    evolution_platina-userdebug \
+    evolution_platina-eng
